56a3b831eb1049950ffdc57d337047a9d8641ae3
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2024-04-16 22:09:08 +0200

    run mobilebert

---
+++ run_mobilebert.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0,1 python train.py \
+        --output_dir ./train_models/mobilebert-cuad \
+        --model_type mobilebert \
+        --model_name_or_path csarron/mobilebert-uncased-squad-v2 \
+        --train_file ./data/train_separate_questions.json \
+        --predict_file ./data/test.json \
+        --do_train \
+        --do_eval \
+        --version_2_with_negative \
+        --learning_rate 1e-4 \
+        --num_train_epochs 4 \
+        --per_gpu_eval_batch_size=40  \
+        --per_gpu_train_batch_size=40 \
+        --max_seq_length 384 \
+        --max_answer_length 384 \
+        --doc_stride 192 \
+        --save_steps 1000 \
+        --n_best_size 20 \
+        --overwrite_output_dir
