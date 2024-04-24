0705ef79e2a82ef896991cf0359a9ed2cf017aff
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2024-04-24 16:15:16 +0200

    gpu --> 1 for eval to run

--- run_mobilebert.sh
+++ run_mobilebert.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 python train.py \
+CUDA_VISIBLE_DEVICES=0 python train.py \
         --output_dir ./train_models/mobilebert-cuad \
         --model_type mobilebert \
         --model_name_or_path csarron/mobilebert-uncased-squad-v2 \
